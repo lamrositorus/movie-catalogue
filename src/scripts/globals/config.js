a83045af108af5594e3383ca8f56ecd49a198459
Lamro Sitorus <lamrostr03@gmail.com>
2024-11-02 09:14:31 +0700

    Initial commit

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '1599d43c85aba223617cb094594a9695',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
